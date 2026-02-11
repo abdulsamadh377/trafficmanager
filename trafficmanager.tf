472a2aab40d1553d0fff64125241bb924e7d1df9
Mohammed Abdul SamadMasluddin <abdulsamadh377@gmail.com>
2026-02-11 08:23:06 +0530

    Update trafficmanager.tf

--- trafficmanager.tf
+++ trafficmanager.tf
@@ -15,21 +15,19 @@ resource "azurerm_traffic_manager_profile" "tm" {
     path     = "/"
   }
 }
-resource "azurerm_traffic_manager_endpoint" "primary" {
+resource "azurerm_traffic_manager_azure_endpoint" "primary" {
   name                = "primary-endpoint"
   profile_name        = azurerm_traffic_manager_profile.tm.name
   resource_group_name = azurerm_resource_group.rg.name
 
-  type               = "azureEndpoints"
   target_resource_id = azurerm_public_ip.primary.id
   priority           = 1
 }
-resource "azurerm_traffic_manager_endpoint" "secondary" {
+resource "azurerm_traffic_manager_azure_endpoint" "secondary" {
   name                = "secondary-endpoint"
   profile_name        = azurerm_traffic_manager_profile.tm.name
   resource_group_name = azurerm_resource_group.rg.name
 
-  type               = "azureEndpoints"
   target_resource_id = azurerm_public_ip.secondary.id
   priority           = 2
 }

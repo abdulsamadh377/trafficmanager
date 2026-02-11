c76c4c56ff539b6caef08ccffcea077356f55b1a
Mohammed Abdul SamadMasluddin <abdulsamadh377@gmail.com>
2026-02-11 08:29:59 +0530

    Add files via upload

---
+++ publicip.tf
@@ -0,0 +1,17 @@
+resource "azurerm_public_ip" "primary" {
+  name                = "pip-primary"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_public_ip" "secondary" {
+  name                = "pip-secondary"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}

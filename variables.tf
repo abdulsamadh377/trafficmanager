b288795d390000949a3aa2e75dcce1981d7b499c
Mohammed Abdul SamadMasluddin <abdulsamadh377@gmail.com>
2026-02-11 09:00:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,10 @@ variable "admin_username" {
 variable "admin_password" {
   default = "Password@123456"   # change in real projects
 }
+locals {
+  common_tags = {
+    environment = "dev"
+    owner       = "terraform"
+    project     = "traffic-manager-demo"
+  }
+}
